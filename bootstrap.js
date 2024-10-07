b9a238f88db1cc2779cc0d23bf0e55d8cf02e9a2
Skander Chaherli <chaherlimedskander@gmail.com>
2024-10-07 14:22:03 +0100

    bootstap

---
+++ bootstrap.js
@@ -0,0 +1,15 @@
+import {
+    Collapse,
+    Ripple,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Collapse, Ripple });
+
+  
+import {
+    Carousel,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Carousel });
